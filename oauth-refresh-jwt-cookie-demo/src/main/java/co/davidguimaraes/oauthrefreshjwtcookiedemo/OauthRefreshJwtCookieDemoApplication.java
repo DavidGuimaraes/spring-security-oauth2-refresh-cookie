0c0a8104149070f389c72c9a9cbfd7c4efc9060b
David Guimaraes <david.ce@live.com>
2020-04-03 06:25:23 -0400

    01. Initializing project.

---
+++ oauth-refresh-jwt-cookie-demo/src/main/java/co/davidguimaraes/oauthrefreshjwtcookiedemo/OauthRefreshJwtCookieDemoApplication.java
@@ -0,0 +1,13 @@
+package co.davidguimaraes.oauthrefreshjwtcookiedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthRefreshJwtCookieDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthRefreshJwtCookieDemoApplication.class, args);
+	}
+
+}
---
+++ oauth-refresh-jwt-cookie-demo/src/test/java/co/davidguimaraes/oauthrefreshjwtcookiedemo/OauthRefreshJwtCookieDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.davidguimaraes.oauthrefreshjwtcookiedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthRefreshJwtCookieDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
